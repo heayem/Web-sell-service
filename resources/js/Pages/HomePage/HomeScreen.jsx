39e1893204bb121ab07d21c6bd70b65875b9d455
sok-leaphea <leapheas2@gmail.com>
2024-11-18 20:10:13 +0700

    task: hero section components

--- resources/js/Pages/HomePage/HomeScreen.jsx
+++ resources/js/Pages/HomePage/HomeScreen.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Layout from "@/Components/Layout/public/Layout";
-import HeroSection from "@/Components/Hero/HeroSection";
+import {HeroWithContent, HeroWithDescription, HeroWithoutContent} from "@/Components/Hero/HeroSection";
 
 export default function HomeScreen() {
     return (
         <>
             <Layout
                 heroSection={
-                    <HeroSection
+                    <HeroWithContent
                         image="./images/image-one.jpg"
                         title="WELCOME TO SERVICEBOX"
                         description="At SERVICEBOX, we provide a seamless platform for businesses to purchase the products they need to thrive. Whether you're looking for essential tools, equipment, or specialized solutions for your company, we are here to make the process easy and efficient."
--- resources/js/Pages/HomePage/Index.jsx
+++ resources/js/Pages/HomePage/Index.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import Layout from "@/Components/Layout/public/Layout";
-import HeroSection from "@/Components/Hero/HeroSection";
+import HomeScreen from "./Homescreen";
 
-export default function HomeScreen() {
+export default function Index() {
     return (
         <>
-            <Layout
-                heroSection={
-                    <HeroSection
-                        image="./images/image-one.jpg"
-                        title="WELCOME TO SERVICEBOX"
-                        description="At SERVICEBOX, we provide a seamless platform for businesses to purchase the products they need to thrive. Whether you're looking for essential tools, equipment, or specialized solutions for your company, we are here to make the process easy and efficient."
-                        isButton
-                        buttonLabel="Get Started"
-                        url="#"
-                    />
-                }
-            />
+            <HomeScreen />
         </>
     );
 }
