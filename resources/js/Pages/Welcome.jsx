85e49d3a61a22674a07e5d4d030774758f5209b0
sok-leaphea <leapheas2@gmail.com>
2024-11-18 20:06:10 +0700

    task: combine hero section components

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,14 +1,9 @@
-import HeroSection from "@/Components/HeroSection";
+import HeroSection from '@/Components/HeroSection';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
-            <HeroSection
-            title = "WELCOME TO SERVICEBOX"
-            description = "At SERVICEBOX, we provide a seamless platform for businesses to purchase the products they need to thrive. Whether you're looking for essential tools, equipment, or specialized solutions for your company, we are here to make the process easy and efficient."
-            isButton
-            buttonLabel = "Get Started"
-            />
+        <HeroSection/>
         </>
     );
 }
