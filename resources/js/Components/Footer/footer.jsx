bf4ede64baef44c0e9eee422d5ebae4cb7fa3a7e
Uthnarin <uthrinrin@gmail.com>
2024-12-02 14:08:32 +0700

    task: footer

--- resources/js/Components/Footer/footer.jsx
+++ resources/js/Components/Footer/footer.jsx
@@ -2,35 +2,36 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-primary text-white py-6">
+        <footer className="bg-primary text-white py-6 md:py-8">
             <div className="container mx-auto">
                 <div className="flex flex-wrap justify-between text-center md:text-left ml-4">
                 <div className="w-full md:w-1/4 mb-4 flex flex-col  justify-center">
                     <h5 className="font-semibold">Company</h5>
                     </div>
 
-                    <div className="w-full md:w-1/4 mb-4 ">
-                        <h5 className="font-semibold mb-2">Support</h5>
+                    <div className="w-full md:w-1/4 mb-4 mt-3 ">
+                        <h5 className="font-semibold mb-2">Feature</h5>
                         <ul>
-                            <li><a href="#" className="hover:underline">Help Center</a></li>
-                            <li><a href="#" className="hover:underline">Contact Us</a></li>
-                            <li><a href="#" className="hover:underline">FAQs</a></li>
+                            <li><a href="#" className="hover:underline">Blog Service</a></li>
+                            <li><a href="#" className="hover:underline">Quality</a></li>
+                          
                         </ul>
                     </div>
-                    <div className="w-full md:w-1/4 mb-4">
+                    <div className="w-full md:w-1/4 mb-4 mt-3">
                         <h5 className="font-semibold mb-2">Services</h5>
                         <ul>
-                            <li><a href="#" className="hover:underline">Web Development</a></li>
-                            <li><a href="#" className="hover:underline">SEO</a></li>
-                            <li><a href="#" className="hover:underline">Marketing</a></li>
+                            <li><a href="#" className="hover:underline">Education</a></li>
+                            <li><a href="#" className="hover:underline">E-Commerce</a></li>
+                            <li><a href="#" className="hover:underline">System Delivery</a></li>
+                            <li><a href="#" className="hover:underline">Hospital System</a></li>
+                            <li><a href="#" className="hover:underline">Illness System</a></li>
                         </ul>
                     </div>
-                    <div className="w-full md:w-1/4 mb-4">
-                        <h5 className="font-semibold mb-2">Follow Us</h5>
+                    <div className="w-full md:w-1/4 mb-4 mt-3">
+                        <h5 className="font-semibold mb-2">Contact</h5>
                         <ul>
-                            <li><a href="#" className="hover:underline">Facebook</a></li>
-                            <li><a href="#" className="hover:underline">Twitter</a></li>
-                            <li><a href="#" className="hover:underline">LinkedIn</a></li>
+                            <li><a href="#" className="hover:underline">Email:info@servicetech.com</a></li>
+                            <li><a href="#" className="hover:underline">Tel: +855 122223344</a></li>
                         </ul>
                     </div>
                 </div>
